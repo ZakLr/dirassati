002d3d78fb35cb92bb75c4773d3b59ccae8c9da2
ZakLr <z.lourghi@esi-sba.dz>
2025-09-07 21:02:51 +0100

    second commit

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import Step1 from "./step1";
-import Step2 from "./step2";
-import Step3 from "./step3";
-import Step4 from "./step4";
-import Step5 from "./step5";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
+import Step4 from "./Step4";
+import Step5 from "./Step5";
 import SignupRight from "./SignupRight";
 
 export default function SignupPage() {
